2ad2cef3cca6717b2094d415ab0f7015ed6509ba
Giap Le <giaple007@gmail.com>
2021-06-01 13:39:56 +0700

    fix[color&size]

--- src/Components/RadioButtonCustom/RadioColorSelect.js
+++ src/Components/RadioButtonCustom/RadioColorSelect.js
@@ -1,52 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
 
-import RadioGroup from '@material-ui/core/RadioGroup';
-import Radio from '@material-ui/core/Radio';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
-
 const radioContainer = {
-    container: {
-        width: 'auto',
-        borderBottom: '1px solid',
-        padding: '0 0 25px'
-    },
-    formLable: {
-        backgroundColor: '#dc5c5c',
-        padding: '5px',
-        color: 'white',
-        marginLeft: '5px'
-    },
-    radioGroupStyle: {
-        marginLeft: '16px',
-    }
-
-}
+  container: {
+    width: "auto",
+    borderBottom: "1px solid",
+    padding: "0 0 25px",
+  },
+  formLable: {
+    backgroundColor: "#dc5c5c",
+    padding: "5px",
+    color: "white",
+    marginLeft: "5px",
+  },
+  radioGroupStyle: {
+    marginLeft: "16px",
+  },
+  attContentBox: {
+    display: "flex",
+  },
+  attContentColorItem: {
+    width: "40px",
+    height: "40px",
+    border: "1px solid rgba(0, 0, 0, .5)",
+    opacity: "0.5",
+    padding: "8px",
+    marginTop: "5px",
+    marginRight: "5px",
+    display: 'flex'
+  },
+  attContentItemInner: {
+    width: "24px",
+    height: "24px",
+  }
+};
 
 const useStyles = makeStyles(radioContainer);
 
 export default function RadioColorSelect(props) {
-    const classes = useStyles();
-    const [selectedValue, setSelectedValue] = useState('brown');
+  const classes = useStyles();
+  const [selectedValue, setSelectedValue] = useState("brown");
 
-    const handleChange = (event) => {
-        setSelectedValue(event.target.value);
-    };
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
 
-    return (
-        <div className={classes.container}>
-            <FormControl component="fieldset">
-                <FormLabel className={classes.formLable} component="legend">Color</FormLabel>
-                <RadioGroup className={classes.radioGroupStyle} row aria-label="color" name="color" value={selectedValue} style={{ width: 'auto' }} onChange={handleChange}>
-                    <FormControlLabel value="brown" control={<Radio />} label="Brown" />
-                    <FormControlLabel value="red" control={<Radio />} label="Red" />
-                    <FormControlLabel value="purple" disabled control={<Radio />} label="Purple" />
-                    <FormControlLabel value="white" control={<Radio />} label="White" />
-                </RadioGroup>
-            </FormControl>
-        </div>
-    );
+  return (
+    <div className={classes.attContentBox}>
+      <div className={classes.attContentColorItem}><div className={classes.attContentItemInner} style={{backgroundColor: 'brown'}}></div></div>
+      <div className={classes.attContentColorItem}><div className={classes.attContentItemInner} style={{backgroundColor: '#2d64bd'}}></div></div>
+      <div className={classes.attContentColorItem}><div className={classes.attContentItemInner} style={{backgroundColor: '#2d64bd'}}></div></div>
+      <div className={classes.attContentColorItem}><div className={classes.attContentItemInner} style={{backgroundColor: '#2d64bd'}}></div></div>
+    </div>
+  );
 }
--- src/Views/ProductPage/Sections/ProductDetails.js
+++ src/Views/ProductPage/Sections/ProductDetails.js
@@ -1,126 +1,141 @@
-import React, { useState } from "react";
+import React from "react";
 
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 
 import GridContainer from "../../../Components/Grid/GridContainer";
 import GridItem from "../../../Components/Grid/GridItem";
-import RadioColorSelect from "../../../Components/RadioButtonCustom/RadioColorSelect"
-
-import RadioSizeSelect from "../../../Components/RadioButtonCustom/RadioSizeSelect"
+import RadioColorSelect from "../../../Components/RadioButtonCustom/RadioColorSelect";
 
 const productStyles = {
-    branchTitle: {
-        fontSize: '25px',
-        color: 'black',
-        background: 'white',
-        margin: '10px 0 10px',
-        border: '1px solid transparent',
-        backgroundColor: '#db9e48',
-        color: '#f7f5f2',
-        padding: '5px',
-        borderRadius: '10px'
-    },
-    boxNew: {
-        fontSize: '10px',
-        color: 'white',
-        background: 'orange',
-        textAlign: 'center',
-        width: '35px',
-        margin: '10px 0 10px'
-    },
-    productPrice: {
-        color: '#79A8B1',
-        marginLeft: '10px',
-    },
-    radioButton: {
-        paddingTop: '10px',
-        width: '500px',
-        margin: '10px 0 10px'
-    },
-    boxShadow: {
-        boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
-    },
-    minusOne: {
-        backgroundColor: '#7ec5d986',
-        border: 'solid 1px transparent',
-        cursor: 'pointer',
-        height: '30px'
-    },
+  branchTitle: {
+    fontSize: "25px",
+    margin: "10px 0 10px",
+    padding: "5px",
+    fontWeight: "bold",
+    fontFamily: "serif",
+  },
+  attBox: {
+    padding: "70px 0",
+  },
+  attTitle: {
+    opacity: "0.5",
+    fontSize: "18px",
+    textTransform: "uppercase",
+    fontFamily: "serif",
+  },
+  attContentBox: {
+    display: "flex",
+  },
+  attContentSizeItem: {
+    width: "40px",
+    height: "40px",
+    border: "1px solid rgba(0, 0, 0, .5)",
+    opacity: "0.5",
+    padding: "8px",
+    marginTop: "5px",
+    marginRight: "5px",
+    textAlign: "center",
+  },
+  productPrice: {
+    marginLeft: "10px",
+    color: "#cf2b2f",
+    fontSize: "25px",
+    fontFamily: "serif",
+  },
+  radioButton: {
+    paddingTop: "10px",
+    width: "500px",
+    margin: "10px 0 10px",
+  },
+  boxShadow: {
+    boxShadow: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)",
+  },
+  minusOne: {
+    backgroundColor: "#7ec5d986",
+    border: "solid 1px transparent",
+    cursor: "pointer",
+    height: "30px",
+  },
 
-    plusOne: {
-        backgroundColor: '#7ec5d986',
-        border: 'solid 1px transparent',
-        cursor: 'pointer',
-        height: '30px'
-    },
-    showAmount: {
-        width: '50px',
-        border: '1px solid #7ec5d986',
-        height: '30px'
-    },
-    plusAndMinusContainer: {
-        float: 'left',
-        marginRight: '5px',
-    },
-    amountContener: {
-        marginLeft: '55px',
-        marginTop: '20px',
-    },
-    btnAddToCard: {
-        marginLeft: '5px',
-        padding: '2px 10px 2px',
-        background: '#f26a52',
-        border: '2px solid transparent',
-        borderRadius: '5px',
-        color: 'white',
-        height: '30px'
-    },
-    productDetailContainer: {
-        borderBottom: '1px solid',
-        borderLeft: '1px solid',
-        padding: '0 0 25x'
-    }
+  plusOne: {
+    backgroundColor: "#7ec5d986",
+    border: "solid 1px transparent",
+    cursor: "pointer",
+    height: "30px",
+  },
+  showAmount: {
+    width: "50px",
+    border: "1px solid #7ec5d986",
+    height: "30px",
+  },
+  plusAndMinusContainer: {
+    float: "left",
+    marginRight: "5px",
+  },
+  amountContener: {
+    marginLeft: "55px",
+    marginTop: "20px",
+  },
+  btnAddToCard: {
+    marginLeft: "5px",
+    padding: "2px 10px 2px",
+    background: "#f26a52",
+    border: "2px solid transparent",
+    borderRadius: "5px",
+    color: "white",
+    height: "30px",
+  },
+  productDetailContainer: {
+    borderBottom: "1px solid",
+    borderLeft: "1px solid",
+    padding: "0 0 25x",
+  },
 };
 
 const useStyles = makeStyles(productStyles);
-const colorList = ['brown', 'red', 'purple', 'white']
-const title = ['Color', 'Size']
+const colorList = ["brown", "red", "purple", "white"];
+const title = ["Color", "Size"];
 
 export default function SimpleSlider() {
-    const classes = useStyles();
-    return (
-        <div className={classes.section}>
-            <div className={classes.container}>
-                <GridContainer >
-                    <GridItem xs={12}>
-                        <div className={classes.branchTitle}> BRANCH NAME</div>
-                        <div className={classes.productPrice}>$ 1998</div>
-                        <div className={classes.radioButton}>
-                            <RadioColorSelect colorList={colorList} title={title} />
-                        </div>
-                        <div className={classes.radioButton}>
-                            <RadioSizeSelect />
-                        </div>
-                        <div className={classes.amountContener}>
-                            <div className={classes.plusAndMinusContainer} >
-                                <button className={classes.minusOne}>
-                                    -1
-                                </button>
-                                {/* input here */}
-                                <input className={classes.showAmount} />
-
-                                <button className={classes.plusOne}>
-                                    +1
-                                </button>
-                            </div>
-                            <div >
-                                <button className={classes.btnAddToCard}>Add To Card</button>
-                            </div>
-                        </div>
-                    </GridItem>
-                </GridContainer>
+  const classes = useStyles();
+  return (
+    <div className={classes.section}>
+      <div className={classes.container}>
+        <GridContainer>
+          <GridItem xs={12}>
+            <div className={classes.branchTitle}>
+              PRODUCT NAME WITH LONG TEXT TO TEST
             </div>
-        </div>
-    );
+            <div className={classes.productPrice}>Vnd 500000</div>
+
+            <div className={classes.attBox}>
+              <div className={classes.attTitle}>Color</div>
+              <RadioColorSelect/>
+              <div className={classes.attTitle}>Size</div>
+              <div className={classes.attContentBox}>
+                <div className={classes.attContentSizeItem}>S</div>
+                <div className={classes.attContentSizeItem}>M</div>
+                <div className={classes.attContentSizeItem}>L</div>
+                <div className={classes.attContentSizeItem}>XL</div>
+              </div>
+            </div>
+
+            <div className={classes.amountContener}>
+              <div className={classes.plusAndMinusContainer}>
+                <button className={classes.minusOne}>-1</button>
+                {/* input here */}
+                <input className={classes.showAmount} />
+
+                <button className={classes.plusOne}>+1</button>
+              </div>
+              <div>
+                <button className={classes.btnAddToCard}>Add To Card</button>
+              </div>
+            </div>
+          </GridItem>
+        </GridContainer>
+      </div>
+    </div>
+  );
 }
